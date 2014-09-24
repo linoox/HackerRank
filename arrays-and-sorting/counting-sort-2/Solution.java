342b4796d82835c731298d3feb2dfeeb16b85280
linoox <pd2614g@gmail.com>
2014-09-24 19:56:06 -0400

    counting sort 2

---
+++ arrays-and-sorting/counting-sort-2/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int MAX_NUM = 100;
+        Scanner stdin=new Scanner(System.in);
+        int sizeOfInput=stdin.nextInt();
+        int[] inputNumberSet=new int[MAX_NUM];
+        for(int i=0;i<sizeOfInput;i++) 
+        	inputNumberSet[stdin.nextInt()]++;
+        
+        for(int i=0;i<MAX_NUM;i++) {
+            int count = inputNumberSet[i];
+            for (int j=0;j<count;j++) {
+                System.out.print(i+" ");    
+            }   
+        }
+    }
+}
