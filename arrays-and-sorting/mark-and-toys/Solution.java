fd674bb781c8b3cc3946dcb84d36d2e08f1f3af4
linoox <pd2614g@gmail.com>
2014-10-21 23:09:44 -0400

    mark and toys -td

---
+++ arrays-and-sorting/mark-and-toys/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+	static int maxToys(int[] toyCosts, int moneyAvailable) {
+
+
+
+	}
+
+	public static void main() {
+
+		int[] toyCosts = {1, 12, 5, 111, 200, 1000, 10};
+		int moneyAvailable = 50;
+
+		System.out.println(maxToys(toyCosts, moneyAvailable));
+	}
+}
